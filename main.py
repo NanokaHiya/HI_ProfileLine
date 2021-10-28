7430d8eb023a145723e0d3d53e25959b9dc0606e
Haiyin Song <nanokahiya@gmail.com>
2021-10-28 16:54:28 +0800

    fluxDensity function test

--- main.py
+++ main.py
@@ -5,59 +5,72 @@ import numpy as np
 import illustris_python as il
 import matplotlib.pyplot as plt
 
-
+#TODO: take redshift into account!
+######################################################################################################
 #Loading information
-basePath = '/public/furendeng/TNG-100/output'
-fields = ['Velocities','CenterOfMass','NeutralHydrogenAbundance','GFM_Metals','Masses']
 subhalo_id = 42
 snapNum = 99
-z=0
-partType = 0 #Gas particle
+basePath = '/public/furendeng/TNG-100/output'
+######################################################################################################
 
 #Load constants
+fields = ['Velocities','CenterOfMass','NeutralHydrogenAbundance','GFM_Metals','Masses']
+partType = 0 #Gas particle
+delta_v = 1.4 #As compatible resolution of Arecibo, unit = km/s
+
 def snapHeader(basePath,snapNum):
     #Load Header from first chunk
-    #Redundant to some extent. Copied from il.snapshot.loadSubset()
-    #Target: get cosmological constants
+    #Copied from il.snapshot.loadSubset()
+    #Get cosmological constants
     with h5py.File(il.snapshot.snapPath(basePath, snapNum),'r') as f:
         header = dict(f['Header'].attrs.items())
     return header
 
-header42 = snapHeader(basePath,snapNum)
-h = header42['HubbleParam'] #Hubble constant, value subject to change, unit = H0/(100km/s/Mpc)
-delta_v = 1.4 #As compatible resolution of Arecibo, unit = km/s
+sHeader = snapHeader(basePath,snapNum)
+h = sHeader['HubbleParam']
+#print(sHeader)
+z = sHeader['Redshift']
 
+def luminosityDis():
+    #Read subhalo position from groupcat
+    #Return luminosity distance in Mpc
+    subhalo_g = il.groupcat.loadSingle(basePath, snapNum, subhaloID=subhalo_id)
+    #subhalo center position [Mpc]
+    subhaloCM=subhalo_g['SubhaloCM']*h*0.001
+    return np.linalg.norm(subhaloCM,2)
+    
+D = luminosityDis()
 
-def LOSVelocity(velocities,centerofmass):
+######################################################################################################
+
+def LOSVelocity():
     #TODO: check why returning always positive velocity!
     #Takes velocities onto centerofmass direction to get LOS velocity
     #Return array shape (N,) with unit km/s
-    centerofmass_km = centerofmass*3.086*np.power(10,16)*h
-    upper = (velocities*centerofmass_km).sum(1)
-    lower = np.sqrt((centerofmass_km*centerofmass_km).sum(1))
+    CM_km = subhalo['CenterOfMass']*3.086*np.power(10,16)*h
+    upper = (subhalo['Velocities']*CM_km).sum(1)
+    lower = np.sqrt((CM_km*CM_km).sum(1))
     return np.transpose(upper/lower)
 
-def MassHI(HiAbundance,GFM_Metals,Masses):
-    #TODO: change number 19710 with length number of particles
+def MassHI():
     #Caculated by gas mass*HydrogenAbundance(from GFM_Metals)*NeutralHydrogenAbundance
     #Return array shape (N,) with unit M_dot
-    Masses_Mdot = Masses*h*np.power(10,10)
-    remove = np.arange(1,10)
-    M_x = np.reshape(np.delete(GFM_Metals,remove,axis=1),(19710))
-    return M_x*HiAbundance*Masses_Mdot
+    M_x = np.reshape(np.delete(subhalo['GFM_Metals'],np.arange(1,10),axis=1),(len(list(subhalo['Masses']))))
+    return M_x*subhalo['NeutralHydrogenAbundance']*h*np.power(10,10)*subhalo['Masses']
 
-def fluxDensity(MassHI,centerofmass):
+def fluxDensity(MassHI,D):
     #convert MassHI into flux density (Catinella et al. 2010)
     #Set N(v)=0
-    #use distance to center of mass as luminosity distance (z=0 case)
-    #should Return array shape(N,) with unit Jy
-    centerofmass_Mpc = centerofmass*h*0.001
-    D = np.sqrt((centerofmass_Mpc*centerofmass_Mpc).sum(1))
     return MassHI/(D*D*2.356*np.power(10,5)*delta_v)
 
+######################################################################################################
+subhalo = il.snapshot.loadSubhalo(basePath, snapNum, subhalo_id, partType, fields)
+MassHI = MassHI()
+LOSVelocity = LOSVelocity()
+fluxDensity = fluxDensity(MassHI,D)
 
-
-subhalo42 = il.snapshot.loadSubhalo(basePath, snapNum, subhalo_id, partType, fields)
+'''
+Depreciated
 rad_velocity = LOSVelocity(subhalo42['Velocities'],subhalo42['CenterOfMass'])
 HIMASS = MassHI(subhalo42['NeutralHydrogenAbundance'],subhalo42['GFM_Metals'],subhalo42['Masses'])
 fluxDen = fluxDensity(HIMASS,subhalo42['CenterOfMass'])
@@ -65,10 +78,10 @@ v_max = np.nanmax(rad_velocity)
 v_min = np.nanmin(rad_velocity)
 bins=int((v_max-v_min)/delta_v)
 #print(bins)
+'''
 
-
+######################################################################################################
 #Save histogram as PNG file
-#Note: There is no meaning of the actual numbers here!
-n, bins, patches = plt.hist(fluxDen, weights = HIMASS, bins=300)
-plt.savefig('conversionTest.png',format='png')
-
+#TODO: tags
+n, bins, patches = plt.hist(fluxDensity, weights = MassHI, bins=300)
+plt.savefig('fluxDensity.png',format='png')
