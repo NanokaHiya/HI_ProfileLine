eee77bb9644809a5243a4b6ba1701fd07fe671f2
Haiyin Song <nanokahiya@gmail.com>
2021-11-17 14:14:20 +0800

    class subhalo implentation complete

--- util.py
+++ util.py
@@ -8,10 +8,9 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-
-#I don't think this is useful right now
+#No use yet
+#class coord:
 '''
-class coord:
     def __init__(self, a, b, c, flag='comoving'):
         #Init a point in a given coordinate system
         #These are used to check avaliablity.
@@ -55,25 +54,25 @@ class coord:
     def reToSpherical(self):
         #recenter->spherical
         #TODO
-'''
+    '''
 
 
 class subhalo:
-    ############################################
+    """Read given subhalo from a snapshot & corresponding group catalog.
+       Generate HI profile line looking from (D,theta,phi) from the subhalo center.
+    """
     basePath = '/public/furendeng/TNG-100/output'
-    ############################################
-    outputName = str('snap'+str(snapNum)+'_subhalo'+str(subhalo_id).zfill(7))
-    output = outputName + '.png'
-    dir_name = 'Snap'+str(snapNum)+'/'
-    #Create output path for this snapshot if not exist
-    try:
-        os.mkdir(dir_name)
-    except:
-        pass
 
     def __init__(self,snapNum,subhalo_id):
         self.snapNum = snapNum
         self.subhalo_id = subhalo_id
+        self.outputName = str('snap'+str(self.snapNum)+'_subhalo'+str(self.subhalo_id).zfill(7))
+        self.dir_name = 'Snap'+str(self.snapNum)+'/'
+        #Create output path for this snapshot if not exist
+        try:
+            os.mkdir(self.dir_name)
+        except:
+            pass
         self.snapData = il.snapshot.loadSubhalo(basePath, self.snapNum, self.subhalo_id, 0, ['Velocities','CenterOfMass','NeutralHydrogenAbundance','GFM_Metals','Masses'])
         #Check if self contain Neutrual Hydrogen
         if self.snapData ['count'] == 0:
@@ -92,19 +91,46 @@ class subhalo:
         self.a = self.header['Time']
         #Load information from group catagroy
         self.subhalo_g = il.groupcat.loadSingle(basePath, self.snapNum, subhaloID=self.subhalo_id)
-    
+        self.subhaloCM = self.subhalo_g['SubhaloCM']
+
+
     def luminosityDistance(self):
+        #currently return simply distance from center of comoving coord
+        #return luminosity distance in Mpc
         #TODO: define a luminosity distance by the size of this subhalo
-
+        self.D = np.linalg.norm(self.subhaloCM*self.h*0.001,2) 
     
-    def obsPoint(self,theta,phi):
-        #TODO: 
-
-
+    def obsPoint(self, theta = 0, phi = 0):
+        #with given angle (and luminosity distance) calculate the observation point
+        #spherical->recentered->comoving
+        self.theta = theta
+        self.phi = phi
+        self.obsPoint = np.add(self.subhaloCM,[self.D * np.cos(phi) * np.sin(theta),self.D * np.sin(phi) * np.sin(theta),self.D * np.cos(theta)])
 
     def LOSVelocity(self):
-        #TODO: I'm not sure if I should keep this function but I'll let it be here for a while
+        #self.LOSVector is the new vector pointing from obsPoint to the subhaloCM (approximate)
+        self.LOSVector = (self.subhaloCM-self.obsPoint)*3.086*np.power(10,16)*self.h
+        self.LOSVelocity = np.transpose((((np.sqrt(self.a)*self.subhalo_g['Velocities']-self.subhaloVel)*self.LOSVector).sum(1))/(np.sqrt((self.LOSVector*self.LOSVector).sum(1))))
 
+    def MassHI(self):
+        self.MassHI = np.reshape(np.delete(self.snapData['GFM_Metals'],np.arange(1,10),axis=1),(len(list(self.snapData['Masses']))))* self.snapData['NeutralHydrogenAbundance']*h*np.power(10,10)*self.snapData['Masses']
+
+    def fluxDen(self):
+        self.fluxDen = MassHI*1000*(1+self.z)/(self.D*self.D*2.356*np.power(10,5)*delta_v)
+
+
+    def bins(self):
+        #calculate number of bins for histogram
+        self.bins = int((np.nanmax(self.LOSVelocity) - np.nanmin(self.LOSVelocity))/delta_v)
 
     def drawPlot(self):
         #TODO: should save figure in subdir
+        self.n, self.bins, self.patches = plt.hist(self.LOSVelocity, weights = self.fluxDen, bins=self.bins, histtype='step')
+        plt.title('Snapshot '+str(self.snapNum)+' Subhalo '+str(self.subhalo_id))
+        plt.xlabel('Velocity')
+        plt.ylabel('FluxDen')
+        plt.text(10, 10, '$D = '+str(self.D)+'\ntheta = '+str(self.theta)+'\nphi = '+str(self.phi) + '\nbins = ' + str(self.bins) + '$', fontsize=12)
+        plt.savefig(self.dir_name + self.outputName + '_r'+str(self.D)+'_phi'+str(self.phi)+'_theta'+str(self.theta)+'.png' , format='png')
+
+
+
